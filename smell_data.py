bd6f0e3813aceac51d6e41389f96e520c66c162b
Elise Villella <elisevillella@gmail.com>
2019-11-17 00:35:51 -0500

    Cleaned up smell data exploration code.

--- smell_data.py
+++ smell_data.py
@@ -1,32 +1,84 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from dateutil import parser
+from datetime import datetime
+from dateutil.parser import parse
 
+# helper functions
+
+def data_overview(data_set):
+    """
+    Takes in the smell data, prints column names, shape, value counts
+    """
+    print('Column headings: ')
+    print(data_set.columns)
+    # Index([u'epoch time', u'date & time', u'smell value', 
+    #       u'skewed latitude', u'skewed longitude', u'zipcode', u'smell description', 
+    #       u'symptoms', u'additional comments'],
+    #      dtype='object')
+    print('Shape: ')
+    print(data_set.shape)
+    # (34488, 9)
+    print('Zipcode value counts: ')
+    print(data_set['zipcode'].value_counts())
+    # 15217     5497
+    # 15218     2972
+    # 15206     2721
+    # 15221     1997
+    # 15025     1367
+    # 15213     1313
+    # ...
+    # 15047     1
+    print('Earliest data point date: ')
+    print(data_set['date & time'].min())
+    # June 1, 2016
+    print('Latest data point date: ')
+    print(data_set['date & time'].max())
+    # November 3, 2019
+    print('Smell number value counts: ')
+    print(data_set['smell value'].value_counts())
+    
+    
+
+def plot_geo_data(data_set):
+    """
+    Plot latitude versus longitude for the dataset.
+    """
+    plt.scatter(data_set['skewed latitude'], data_set['skewed longitude'])
+    plt.title('Geographical Distribution of Data Points')
+    plt.xlabel('Latitude')
+    plt.ylabel('Longitude')
+    plt.show()
+    
+def plot_time_vs_smell(data_set):
+    """
+    Plot time only vs smell to see whether all times of day are represented.
+    """
+    times = [dt.time() for dt in data_set['date & time']]
+    plt.scatter(times, data_set['smell value'])
+    plt.title('Time of day versus smell value')
+    plt.xlabel('Time of day')
+    plt.ylabel('Smell value')
+    plt.show()
+    
+    
+
+# load data
 raw_data = pd.read_csv('smell_reports.csv')
-#print(data.head())
-#print(data.columns) = Index([u'epoch time', u'date & time', u'smell value', u'skewed latitude',
-#       u'skewed longitude', u'zipcode', u'smell description', u'symptoms',
-#       u'additional comments'],
-#      dtype='object')
-#data.shape = (34488, 9)
 
-# Figure out which zipcodes to consider - some seem to have much more data than others
-#zipcodes = raw_data['zipcode'].unique()
-#print(raw_data['zipcode'].value_counts())
 
-#plot latitude, longitude to estimate geographic spread of data
-plt.scatter(raw_data['skewed latitude'], raw_data['skewed longitude'])
-plt.title('geographical distribution of data points')
-plt.show()
+# Initial data prep
+raw_data['date & time'] = pd.to_datetime(raw_data['date & time'])
 
 
 
 
 
-## code for scatter plot
-#plt.scatter(raw_data['zipcode'], raw_data['smell value'])
-#plt.title('Zipcode vs smell value')
-#plt.xlabel('zipcode')
-#plt.ylabel('smell value')
-#plt.show()
+
+
+
+
+
+
+
+#data_overview(raw_data)
